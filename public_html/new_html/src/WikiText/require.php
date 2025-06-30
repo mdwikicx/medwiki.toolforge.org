5696913a7f4306efb366b917ffc8a389127b5145
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-06-30 03:46:39 +0300

    Refactor require.php to include WikiParse dependencies dynamically and create a new require.php for WikiParse

--- public_html/new_html/src/WikiText/require.php
+++ public_html/new_html/src/WikiText/require.php
@@ -1,9 +1,11 @@
 <?php
 
-include_once __DIR__ . '/WikiParse/Template.php';
-include_once __DIR__ . '/WikiParse/Citations.php';
-include_once __DIR__ . '/WikiParse/Category.php';
+include_once __DIR__ . '/WikiParse/require.php';
 
+foreach (glob(__DIR__ . "/fixes/*.php") as $filename) {
+    include_once $filename;
+}
+/*
 include_once __DIR__ . '/fixes/fix_images.php';
 include_once __DIR__ . '/fixes/fix_cats.php';
 include_once __DIR__ . '/fixes/del_temps.php';
@@ -13,5 +15,5 @@ include_once __DIR__ . '/fixes/fix_langs_links.php';
 include_once __DIR__ . '/fixes/del_mt_refs.php';
 include_once __DIR__ . '/fixes/expend_refs.php';
 include_once __DIR__ . '/fixes/ref_work.php';
-
+*/
 require_once __DIR__ . "/fix_wikitext.php";
