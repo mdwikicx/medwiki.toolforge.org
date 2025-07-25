7d1413f762f25ceab53aa2dedd34507c04d85bf5
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-07-25 04:50:04 +0300

    Update revisions_new.php

--- public_html/new_html/revisions_new.php
+++ public_html/new_html/revisions_new.php
@@ -201,7 +201,7 @@ $re_create_th = (isset($_GET['re'])) ? "<th>Re create</th>" : '';
 ?>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light-subtle">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">MDWiki</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
