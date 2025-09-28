a3038a564ff903284f8e24202a92a5be2cf566ac
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-09-28 03:22:05 +0300

    Update dump_import.sh

--- dump_import.sh
+++ dump_import.sh
@@ -3,7 +3,6 @@
 cd public_html/mediawiki
 
 php maintenance/run.php importDump.php <  ~/public_html/Translation_Tool-20250920013003.xml
-php maintenance/run.php rebuildrecentchanges.php
-php maintenance/run.php initSiteStats.php
-php maintenance/run.phpinitSiteStats.php --update
+php maintenance/run.php rebuildrecentchanges.php --update
+php maintenance/run.php initSiteStats.php --update
 php maintenance/run.php
