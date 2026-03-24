ec79eeac558b9528db59fc4cc1eba098cbfb111a
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-25 02:30:47 +0300

    .

--- public_html/new_html/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if ((empty($_GET) && empty($_POST)) || (count($_GET) == 1 && isset($_GET["test"]))) {
-    // require_once __DIR__ . "/revisions.html";
-    header("Location: revisions.html");
-} else {
-    require_once __DIR__ . "/main.php";
-}
